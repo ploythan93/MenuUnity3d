895cfa04932a85468a62bda1d62f0a9b5e4d3d2f
Nikita Churikov <nikita@chur.ru>
2014-09-26 23:22:34 +0400

    First

---
+++ Menu/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+	void OnMouseEnter(){
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUpAsButton(){
+		Application.Quit ();
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GameObject.Find("txtQuit").renderer.enabled = true;		
+		}
+		else if (Input.GetKeyDown (KeyCode.Space)){
+			GameObject.Find("txtQuit").renderer.enabled = false;	
+		}
+	}
+}
